9582f964fa645327ccb2be313034cf044c3ad480
Nik O'Brien <niks.programming@gmail.com>
2023-04-21 16:18:21 -0700

    Finish the project solution by adding a reduce enumerable

--- 02-substring-method/substring_method.rb
+++ 02-substring-method/substring_method.rb
@@ -8,12 +8,12 @@ def count_substrings(my_string = "Hello", my_subs = ["Hell", "lo", "l", "x"])
 
     # select only the items that are present in the string
     valid_sub = my_subs.select{ |item| my_string.include? item}
-    valid_sub
-    
+
     # create and iterate a new hash
-
-
-
+    valid_sub.reduce(Hash.new(0)) do |result, sub_str|
+        result[sub_str] = my_string.scan(sub_str).length
+        result
+    end
 end
 
-puts count_substrings()
+puts count_substrings("Howdy partner, sit down! How's it going?", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
