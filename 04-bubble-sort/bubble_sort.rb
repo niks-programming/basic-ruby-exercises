0086fe850fddb372c8a406a961f6e290704c8dbc
Nik O'Brien <niks.programming@gmail.com>
2023-04-25 08:33:57 -0700

    Create the initial enumerable that sorts properly

--- 04-bubble-sort/bubble_sort.rb
+++ 04-bubble-sort/bubble_sort.rb
@@ -1,8 +1,17 @@
 # Nik O'Brien
 # Created Date: 4/23/2023
 
-def bubble_sort(my_ary = [3,1,2])
+def bubble_sort(my_ary = [4,3,1,2])
 
+    # iterate and swap elements accordingly
+    my_ary.each_with_index do |i,j|
+        comparison = (i <=> my_ary[j+1])
+        if comparison == 1
+            my_ary[j], my_ary[j+1] = my_ary[j+1], my_ary[j]
+            puts my_ary 
+        end
+    end
+    
 end
 
 bubble_sort()
