68e8436ed15661cf8d89963d9a3afe27394bcd3e
Nik O'Brien <niks.programming@gmail.com>
2023-04-26 12:14:48 -0700

    Make more efficient by inlcuding a times method iteration

--- 04-bubble-sort/bubble_sort.rb
+++ 04-bubble-sort/bubble_sort.rb
@@ -2,24 +2,10 @@
 # Created Date: 4/23/2023
 
 def bubble_sort(my_ary = [4,3,1,2])
-    # ideal sorted end state array
-    sorted_ary = Array.new(my_ary.length-1, -1)
-    # comparison array 
-    comp_ary = []
-
-    #iterate until our ending comp array matches the ideal sorted array
-    until comp_ary == sorted_ary
-        # iterate and swap elements accordingly
-        my_ary.each_with_index do |i,j|
-            comparison = (i <=> my_ary[j+1])
-            if comparison == 1
-                my_ary[j], my_ary[j+1] = my_ary[j+1], my_ary[j]
-            end
-        end
-
-        # populate the comparison array by mapping the newly sorted array
-        comp_ary = my_ary.each_cons(2).map{|i,j| i <=> j}
-        comp_ary = comp_ary.join(",").gsub("0", "-1").split(",").map{|i| i.to_i}
+    #iterate the amount of times as the length of our array
+    my_ary.length.times do 
+        # swap elements when the rocket operator equals 1
+        my_ary.each_with_index { |i, index| my_ary[index], my_ary[index+1] = my_ary[index+1], my_ary[index] if (i <=> my_ary[index+1]) == 1 }
     end
 
     puts my_ary
