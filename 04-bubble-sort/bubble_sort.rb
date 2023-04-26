9ac7981031a906ebef28a0fd4e2a09407387947f
Nik O'Brien <niks.programming@gmail.com>
2023-04-26 06:38:52 -0700

    Include an until loop, but need to fix repeat element problem

--- 04-bubble-sort/bubble_sort.rb
+++ 04-bubble-sort/bubble_sort.rb
@@ -2,16 +2,27 @@
 # Created Date: 4/23/2023
 
 def bubble_sort(my_ary = [4,3,1,2])
+    # ideal sorted end state array
+    sorted_ary = Array.new(my_ary.length-1, -1)
+    # comparison array 
+    comp_ary = []
 
-    # iterate and swap elements accordingly
-    my_ary.each_with_index do |i,j|
-        comparison = (i <=> my_ary[j+1])
-        if comparison == 1
-            my_ary[j], my_ary[j+1] = my_ary[j+1], my_ary[j]
-            puts my_ary 
+    #iterate until our ending comp array matches the ideal sorted array
+    until comp_ary == sorted_ary
+        # iterate and swap elements accordingly
+        my_ary.each_with_index do |i,j|
+            comparison = (i <=> my_ary[j+1])
+            if comparison == 1
+                my_ary[j], my_ary[j+1] = my_ary[j+1], my_ary[j]
+            end
         end
+
+        # populate the comparison array by mapping the newly sorted array
+        comp_ary = my_ary.each_cons(2).map{|i,j| i <=> j}
     end
-    
+
+    puts my_ary
 end
 
-bubble_sort()
+bubble_sort([4,3,78,2,0])
+#bubble_sort([4,3,78,2,0,2])
